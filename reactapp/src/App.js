c1bdbe2d4f4d3f6aa7184e897c1d1b50f526a66f
Jeong-wonho <kallzzang@naver.com>
2022-10-17 23:06:31 +0900

    Life Cycle 적용

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,16 +1,18 @@
 import "./App.css";
-import ClassComponents from "./components/2-5.State/ClassComponents";
-import Composition from "./components/2-4.Props/Composition";
-import Extraction from "./components/2-4.Props/Extraction/Extraction";
-import FunctionalComponents from "./components/2-5.State/FunctionalComponents";
+// import ClassComponents from "./components/2-5.State/ClassComponents";
+// import Composition from "./components/2-4.Props/Composition";
+// import Extraction from "./components/2-4.Props/Extraction/Extraction";
+// import FunctionalComponents from "./components/2-5.State/FunctionalComponents";
+import ClassComponents2 from "./components/2-6.LifeCycle/ClassComponents";
 
 function App() {
   return (
     <div className="App">
-      <FunctionalComponents />
-      <ClassComponents />
-      <Extraction />
-      <Composition />
+      <ClassComponents2 />
+      {/* <FunctionalComponents /> */}
+      {/* <ClassComponents /> */}
+      {/* <Extraction /> */}
+      {/* <Composition /> */}
     </div>
   );
 }
---
+++ reactapp/src/components/2-6.LifeCycle/ClassComponents.jsx
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+
+export default class ClassComponents extends Component {
+  constructor(props) {
+    super(props);
+    console.log("constructor");
+    this.state = { date: new Date() };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  componentDidMount() {
+    console.log("componentDidMount");
+    this.timerID = setInterval(() => this.tick(), 10000);
+  }
+  componentDidUpdate() {
+    console.log("componentDidUpdate");
+  }
+
+  componentWillUnmount() {
+    console.log("componentWillUnmount");
+    clearInterval(this.timerId);
+  }
+
+  tick() {
+    console.log("tick");
+    this.setState({ date: new Date() });
+  }
+  handleClick() {
+    alert(this.state.date);
+  }
+
+  render() {
+    console.log("render");
+    return (
+      <div>
+        <h1 onClick={this.handleClick}>Hello Life Cycle</h1>
+        <h2>{this.state.date.toLocaleTimeString()}</h2>
+      </div>
+    );
+  }
+}
