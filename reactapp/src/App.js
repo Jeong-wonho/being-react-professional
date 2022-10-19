f9bb2949a758adbfacb512b311aff6014f55e92b
Jeong-wonho <kallzzang@naver.com>
2022-10-19 23:32:20 +0900

    unControlld Component

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,17 +1,21 @@
 import "./App.css";
 // import Condition from "./components/2-8.ConditionalRendering/Condition";
-import List from "./components/2-9.List/List";
+// import List from "./components/2-9.List/List";
 // import ClassComponents from "./components/2-5.State/ClassComponents";
 // import Composition from "./components/2-4.Props/Composition";
 // import Extraction from "./components/2-4.Props/Extraction/Extraction";
 // import FunctionalComponents from "./components/2-5.State/FunctionalComponents";
 // import ClassComponents2 from "./components/2-6.LifeCycle/ClassComponents";
 // import Event from "./components/2-7.Event/Event";
+import ControlledComponent from "./components/2-10.Form/ControlledComponent";
+import UnControlledComponent from "./components/2-10.Form/UnControlledComponent";
 
 function App() {
   return (
     <div className="App">
-      <List />
+      <UnControlledComponent />
+      <ControlledComponent />
+      {/* <List /> */}
       {/* <Condition /> */}
       {/* <Event /> */}
       {/* <ClassComponents2 /> */}
