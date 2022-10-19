4bd94b9fbf7a7704c2f0bcd7a0a15a71471dad08
Jeong-wonho <kallzzang@naver.com>
2022-10-19 22:07:42 +0900

    이벤트 캡처링, 버블링, 합성이벤트

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -3,12 +3,14 @@ import "./App.css";
 // import Composition from "./components/2-4.Props/Composition";
 // import Extraction from "./components/2-4.Props/Extraction/Extraction";
 // import FunctionalComponents from "./components/2-5.State/FunctionalComponents";
-import ClassComponents2 from "./components/2-6.LifeCycle/ClassComponents";
+// import ClassComponents2 from "./components/2-6.LifeCycle/ClassComponents";
+import Event from "./components/2-7.Event/Event";
 
 function App() {
   return (
     <div className="App">
-      <ClassComponents2 />
+      <Event />
+      {/* <ClassComponents2 /> */}
       {/* <FunctionalComponents /> */}
       {/* <ClassComponents /> */}
       {/* <Extraction /> */}
