248e48bc12cc03a521fa818fd6c3c9bd0b2aa3b7
Jeong-wonho <kallzzang@naver.com>
2022-11-17 23:49:18 +0900

    Portal

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import Example from "./components/3-11. Portal/Example";
 
 // import Condition from "./components/2-8.ConditionalRendering/Condition";
 // import List from "./components/2-9.List/List";
@@ -17,7 +18,7 @@ import "./App.css";
 // import Input from "./components/3-7. HOC/Input";
 // import Button from "./components/3-7. HOC/Button";
 // import Memo from "./components/3-8. Memoization/Memo";
-import Example from "./components/3-10. Context/Example";
+// import Example from "./components/3-10. Context/Example";
 
 function App() {
   return (
