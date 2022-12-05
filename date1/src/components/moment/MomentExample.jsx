547256636539e5dd04a0510666fbc9a1ac26369c
Jeong-wonho <kallzzang@naver.com>
2022-12-05 22:47:30 +0900

    moment.js

--- date1/src/components/moment/MomentExample.jsx
+++ date1/src/components/moment/MomentExample.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import moment from "moment-timezone";
+import "moment/locale/ko";
 
 export default function MomentExample() {
+  const birthDayRef = useRef(null);
+  const [day, setDay] = useState("");
+  const handleBirthDayChange = (event) => {
+    setDay(moment(event.target.value, "YYYY-MM-DD").format("dddd"));
+  };
+
   const momentDate = moment();
   const newMomentdate = momentDate.add(1, "week");
   const cloneNewMomentDate = newMomentdate.clone().add(1, "week");
@@ -33,9 +40,17 @@ export default function MomentExample() {
         {moment("07-17-2021").format("YYYY M월 D일")}
       </div>
       <br />
-      <div>두 날짜가 비교</div>
+      <div> 자기 생일 요일 찾기 </div>
       <div>
-        <input type="text" ref="birthDayRef" />
+        <input type="date" ref={birthDayRef} onChange={handleBirthDayChange} />
+        <div>무슨 요일이였을까?</div>
+        <div>{day}</div>
+      </div>
+      <br />
+      <div>두 날짜 비교</div>
+      <div>2021-07-17 03:00 와 2021-07-18 02:00은 몇시간 차이인가?</div>
+      <div>
+        {moment("2021-07-17 03:00").diff(moment("2021-07-18 02:00"), "hours")}
       </div>
     </div>
   );
