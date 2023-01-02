ba2e037cf01344f897b3ef56388ad6addbfec1f2
Jeong-wonho <kallzzang@naver.com>
2023-01-02 21:09:35 +0900

    styled 마무리

--- styling/src/App.js
+++ styling/src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 // import StyledComponentExample from "./components/StyledComponentsExample/StyledComponentExample";
-import EmotionExample from "./components/EmotionExample/EmotionExample";
+// import EmotionExample from "./components/EmotionExample/EmotionExample";
+import Example from "./components/SassExample/Example";
 function App() {
   return (
     <div className="App">
       {/* <StyledComponentExample /> */}
-      <EmotionExample />
+      {/* <EmotionExample /> */}
+      <Example />
     </div>
   );
 }
--- styling/src/components/EmotionExample/EmotionExample.jsx
+++ styling/src/components/EmotionExample/EmotionExample.jsx
@@ -1,3 +1,4 @@
+import { keyframes } from "styled-components";
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react";
 
@@ -69,6 +70,24 @@ const base = css`
   color: turquoise;
 `;
 
+const bounce = keyframes`
+  from, 20%, 53%, 80%, to {
+    transform: translate3d(0,0,0);
+  }
+
+  40%, 43% {
+    transform: translate3d(0, -30px, 0);
+  }
+
+  70% {
+    transform: translate3d(0, -15px, 0);
+  }
+
+  90% {
+    transform: translate3d(0,-4px,0);
+  }
+`;
+
 export default function EmotionExample() {
   return (
     <>
@@ -97,6 +116,23 @@ export default function EmotionExample() {
           danger styles.
         </div>
         <div css={[base, danger]}>This will be red</div>
+        <p
+          css={css`
+            font-size: 30px;
+            @media (min-width: 420px) {
+              font-size: 50px;
+            }
+          `}
+        >
+          Some text!
+        </p>
+      </div>
+      <div
+        css={css`
+          animation: ${bounce} 1s ease infinite;
+        `}
+      >
+        some bouncing text!
       </div>
     </>
   );
