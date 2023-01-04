201c683fa488f50754f4073c07c304ce6c559a87
Jeong-wonho <kallzzang@naver.com>
2023-01-04 23:22:13 +0900

    ant design

---
+++ ui2/src/components/AntDesign/AntDesignExample.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import FormExample from "./FormExample";
+// import GetStart from "./GetStart";
+// import IconExaple from "./IconExaple";
+// import TypographyExample from "./TypographyExample";
+import LayoutExample from "./LayoutExample";
+import ModalExample from "./ModalExample";
+import PageExample from "./PageExample";
+
+export default function AntDesignExample() {
+  return (
+    <div>
+      <ModalExample />
+      <FormExample />
+      <PageExample />
+      <LayoutExample />
+      {/* <TypographyExample />
+      <IconExaple />
+      <GetStart /> */}
+    </div>
+  );
+}
