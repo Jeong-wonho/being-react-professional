ea9a3a79cda520ced32bb772b96d05b574055ea7
Jeong-wonho <kallzzang@naver.com>
2023-01-10 22:02:12 +0900

    react bootstrap & materialui

--- ui2/src/App.js
+++ ui2/src/App.js
@@ -3,17 +3,20 @@
 // import "onsenui/css/onsen-css-components.css";
 // import OnsenUIExample from "./components/OnsenUI/OnsenUIExample";
 // import AntDesignExample from "./components/AntDesign/AntDesignExample";
-import "antd/dist/reset.css";
-import "./App.css";
-import ReactSemanticUIExample from "./components/ReactSemantic/ReactSemanticUIExample";
+// import "antd/dist/reset.css";
+// import ReactSemanticUIExample from "./components/ReactSemantic/ReactSemanticUIExample";
+import ReactBootstrapExample from "./components/ReactBootstrap/ReactBootstrapExample";
+import "bootstrap/dist/css/bootstrap.min.css";
 
+import "./App.css";
 function App() {
   return (
     // <div className="App">
     // <OnsenUIExample />
     // <AntDesignExample />
     // </div>
-    <ReactSemanticUIExample />
+    // <ReactSemanticUIExample />
+    <ReactBootstrapExample />
   );
 }
 
---
+++ ui2/src/components/ReactBootstrap/ReactBootstrapExample.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+// import ButtonExample from "./ButtonExample";
+// import AlertExample from "./AlertExample";
+// import GridExample from "./GridExample";
+// import CarouselExample from "./CarouselExample";
+import MydModalWithGrid from "./MyModalWithGrid";
+
+export default function ReactBootstrapExample() {
+  return (
+    <div>
+      <MydModalWithGrid />
+      {/* <CarouselExample /> */}
+      {/* <ButtonExample /> */}
+      {/* <AlertExample /> */}
+      {/* <GridExample /> */}
+    </div>
+  );
+}
